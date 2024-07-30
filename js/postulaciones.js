330931c162e9f347a3d6f37cc3e10c968dab74d9
dariast03 <danteariastarifa@gmail.com>
2024-07-30 17:00:40 -0400

    :(?

--- js/postulaciones.js
+++
@@ -1,9 +0,0 @@
-function confirmLogout(event) {
-    if (!confirm("¿Estás seguro de que quieres cerrar sesión?")) {
-        event.preventDefault();
-    }
-}
-
-window.onload = function() {
-    document.getElementById('logout-link').addEventListener('click', confirmLogout);
-};Zzz
